e66ae8309b17dfb71da81367d6e035f7624f7740
jiwonee02 <nepthun@naver.com>
2024-07-02 15:36:25 +0900

    불필요 파일 삭제

--- Here_sooyeon/java/home.js
+++
@@ -1,9 +0,0 @@
-function goToHistoryPage() {
-  window.location.href = "../html/history.html";
-}
-function goToArchivePage() {
-  window.location.href = "../html/archive.html";
-}
-function goToCommunityPage() {
-  window.location.href = "../html/community.html";
-}
