4ffe1e8d09bbe938710c5934c2150a04b2d5ce18
haereum00 <phr8048@naver.com>
2024-07-04 15:45:52 +0900

    Revert "Archive"

--- musicarchive/admin.py
+++
@@ -1,6 +0,0 @@
-# musicarchive/admin.py
-
-from django.contrib import admin
-from .models import MusicArchive
-
-admin.site.register(MusicArchive)
--- musicarchive/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MusicarchiveConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'musicarchive'
