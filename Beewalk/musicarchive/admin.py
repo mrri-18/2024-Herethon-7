16cccb49c6a913cbe539f2f9216892482b876e04
haereum00 <phr8048@naver.com>
2024-07-05 13:57:54 +0900

    음악

--- Beewalk/musicarchive/admin.py
+++ Beewalk/musicarchive/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+# musicarchive/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import MusicArchive
+
+admin.site.register(MusicArchive)
