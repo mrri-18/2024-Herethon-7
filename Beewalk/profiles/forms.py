4ac26e726bf606c54e2563241428ef60e41dc3db
mrri-18 <emk0528@swu.ac.kr>
2024-07-06 00:17:51 +0900

    프로필편집 폼 수정 완료

--- Beewalk/profiles/forms.py
+++ Beewalk/profiles/forms.py
@@ -2,11 +2,18 @@ from django import forms
 from Accountapp.models import Member
 
 class MemberUpdateForm(forms.ModelForm):
+    username = forms.CharField(
+        label='아이디',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '아이디를 입력해주세요'})
+    )
+    email = forms.EmailField(
+        label='이메일',
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': '이메일을 입력해주세요'})
+    )
+
     class Meta:
         model = Member
         fields = ['username', 'email', 'profile_img']
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'profile_img': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
